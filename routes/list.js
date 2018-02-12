0d51a7d49a6af86d5564b8f9c42ec7f01812477f
mtopolinski <35231669+mtopolinski@users.noreply.github.com>
2018-02-11 18:02:28 -0600

    Add files via upload

--- routes/list.js
+++ routes/list.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET list. */
 router.get('/', function(req, res, next) {
     res.render('list', { title: 'List'});
-    res.send('this is my list page');
 });
 
 module.exports = router;
+
